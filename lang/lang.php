930fc25050969fc66d3531e2406dff26c1e9c020
Jacques Malgrange <jacques.malgrange@gmail.com>
2017-03-15 19:32:00 +0100

    V1.2.2

--- lang/lang.php
+++ lang/lang.php
@@ -1,16 +1,20 @@
 <?php
-	$langPlug = array(
-		"fr" => "fr_FR.utf8",
-		"en" => "en_US",
-		"es" => "es_ES.utf8"
-		);
-	//	
-	if(isset($langPlug[$lang]) && $langPlug[$lang])
-		{
-		require_once(dirname(__FILE__).'/../../../includes/lang/php-gettext/gettext.inc');
-		T_setlocale(LC_MESSAGES, $langPlug[$lang]);
-		T_bindtextdomain("payplug", dirname(__FILE__));
-		T_bind_textdomain_codeset("payplug", "UTF-8");
-		T_textdomain("payplug");
-		}
+$langPlug = array(
+	"fr" => "fr_FR.utf8",
+	"en" => "en_US",
+	"es" => "es_ES.utf8"
+	);
+//	
+if(!empty($langPlug[$lang]))
+	{
+	require_once(dirname(__FILE__).'/../../../includes/lang/php-gettext/gettext.inc');
+	T_setlocale(LC_MESSAGES, $langPlug[$lang]);
+	T_bindtextdomain("payplug", dirname(__FILE__));
+	T_bind_textdomain_codeset("payplug", "UTF-8");
+	T_textdomain("payplug");
+	}
+else if(!function_exists('T_'))
+	{
+	function T_($f) { return $f; }
+	}
 ?>
